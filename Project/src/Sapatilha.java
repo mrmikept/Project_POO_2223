b78ab62d6898a2bf86f784184572b0a09492370d
mrmikept <mike.pinto1507@gmail.com>
2023-04-24 14:46:59 +0100

    Alteração formula calculo desconto

--- Project/src/Sapatilha.java
+++ Project/src/Sapatilha.java
@@ -11,7 +11,7 @@ public class Sapatilha extends Artigo {
     private int tamanho;
     private int tipoCordao;
     private String cor;
-    private LocalDate data;
+    private LocalDate dataLancamento;
     private int tipo;
 
     public static final int CORDAO = 0;
@@ -19,33 +19,30 @@ public class Sapatilha extends Artigo {
     public static final int NORMAL = 0;
     public static final int PREMIUM = 1;
 
-    public Sapatilha()
-    {
+    public Sapatilha() {
         super();
         this.tamanho = 0;
         this.tipoCordao = CORDAO;
         this.cor = "";
-        this.data = LocalDate.now();
+        this.dataLancamento = LocalDate.now();
         this.tipo = NORMAL;
     }
 
-    public Sapatilha(int id, String descricao, String marca, double precoBase, double correcaoPreco, EstadoArtigo estado, Transportadora transportadora, int tamanho, int tipoCordao, String cor, LocalDate data, int tipo)
-    {
+    public Sapatilha(int id, String descricao, String marca, double precoBase, double correcaoPreco, EstadoArtigo estado, Transportadora transportadora, int tamanho, int tipoCordao, String cor, LocalDate dataLancamento, int tipo) {
         super(id, descricao, marca, precoBase, correcaoPreco, estado, transportadora);
         this.tamanho = tamanho;
         this.tipoCordao = tipoCordao;
         this.cor = cor;
-        this.data = data;
+        this.dataLancamento = dataLancamento;
         this.tipo = tipo;
     }
 
-    public Sapatilha(Sapatilha sapatilha)
-    {
+    public Sapatilha(Sapatilha sapatilha) {
         super(sapatilha);
         this.tamanho = sapatilha.getTamanho();
         this.tipoCordao = sapatilha.getTipoCordao();
         this.cor = sapatilha.getCor();
-        this.data = sapatilha.getData();
+        this.dataLancamento = sapatilha.getDataLancamento();
         this.tipo = sapatilha.getTipo();
 
     }
@@ -74,12 +71,12 @@ public class Sapatilha extends Artigo {
         this.cor = cor;
     }
 
-    public LocalDate getData() {
-        return data;
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
     }
 
     public int getTipo() {
@@ -90,21 +87,17 @@ public class Sapatilha extends Artigo {
         this.tipo = tipo;
     }
 
-    public double getCorrecaoPreco()
-    {
-        if (this.getEstado().getTipoEstado() == EstadoArtigo.USADO && this.getEstado().getNrDonos() <= 10)
+    public double getCorrecaoPreco() {
+        if (this.getEstado().getTipoEstado() == EstadoArtigo.USADO)
         {
-            return (1 - ((((double)this.getEstado().getNrDonos() / 11) * (1- ((double)this.getEstado().getAvaliacao() / 6)))));
-        } else if (this.getEstado().getTipoEstado() == EstadoArtigo.USADO && this.getEstado().getNrDonos() > 10) {
-            return (1 - (((10.0 / 11) * (1- ((double)this.getEstado().getAvaliacao() / 6)))));
+            return  -this.getPrecoBase() * (double) Math.round((((1 - (this.getEstado().getAvaliacao() / 5.0)) * 0.6) + ((Math.pow((2),(-this.getEstado().getNrDonos())) * (-1) + 1) * 0.4)) * 100.0) / 100.0;
         } else if (this.getEstado().getTipoEstado() == EstadoArtigo.NOVO && this.getTamanho() > 45) {
-            return 0.3;
+            return this.getPrecoBase() * (-0.2);
         }
         return 0;
     }
 
-    public Sapatilha clone()
-    {
+    public Sapatilha clone() {
         return new Sapatilha(this);
     }
 }
